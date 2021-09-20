43518fe4b3771325263bd59332ba16395387b503
DESKTOP-DC2R3RQ\lrnzm <Lorenzo Amodeo>
2021-09-20 10:52:39 +0200

    end

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
-const express = require('express');
-const app = express();
+require('dotenv').config()
 
-app.listen(3000, () => console.log('Server is start'));
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
+
+app.listen(3000, () => console.log('Server Started'))
